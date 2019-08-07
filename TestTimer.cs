4d934d43cb7b81c32eb4402029b8cf0ecd886a62
brianvarskonst <brianvar.skonst@gmail.com>
2019-08-07 10:15:11 +0200

    Refactoring & Create Test Class
    Adjust Timer Class Namespace, make it more clearer.
    
    Create new MonoBehaviour for testing the UnityTimer

---
+++ TestTimer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+using UnityTimer;
+
+public class TestTimer : MonoBehaviour
+{
+    private void Start()
+    {
+       Timer.Create(TestingAction, 3f, "Timer");
+            
+       Timer.Create(StopTestingAction, 6f, "StopTimer");
+            
+//            Timer.Stop("Timer");
+    }
+
+    private void TestingAction()
+    {
+        Debug.Log("Testing");
+    }
+        
+    private void StopTestingAction()
+    {
+        Debug.Log("Stop Testing");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Timer.Create(() => TestingAction(), 1f);
+        }
+    }
+}
